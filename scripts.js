9fb60489f2ff05b9b438cd7fc5a8c76fad4f4c56
Marcus Neves <marcus-neves@outlook.com>
2023-09-19 14:40:23 -0300

    fix: apontamento do scripts.js e slashbars das imagens

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-  function addDivToContainer() {
-      var newDiv = document.createElement("div");
+function addDivToContainer() {
+      const newDiv = document.createElement("div");
       newDiv.setAttribute('class', 'message sender');
       newDiv.textContent = "clicado";
-      var container = document.getElementById("container");
+      const container = document.getElementById("container");
       container.appendChild(newDiv);
   }
