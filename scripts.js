f949f7898d8386cc812b53c472312afb86ea8562
donmarx <marx.maciel2013@gmail.com>
2023-09-20 13:30:04 -0300

    modified: script.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-
-  function addDivToContainer(clickedId) {
+function addDivToContainer(clickedId) {
       var newDiv = document.createElement("div");
       newDiv.setAttribute('class', 'message sender');
       switch (clickedId){
@@ -19,16 +18,10 @@
           "algo de errado"
       }
       var container = document.getElementById("container");
-
-function addDivToContainer() {
-      const newDiv = document.createElement("div");
-      newDiv.setAttribute('class', 'message sender');
-      newDiv.textContent = "clicado";
-      const container = document.getElementById("container");
       container.appendChild(newDiv);
 
   }
 
-  $( document ).ready(function() {
+$( document ).ready(function() {
     console.log( "ready!" );
 });
