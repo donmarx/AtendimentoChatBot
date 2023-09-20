f7a14490ca3388d0322125a496473ef54560d5cd
donmarx <marx.maciel2013@gmail.com>
2023-09-20 12:03:33 -0300

    Botoes funcionando perfeitamente

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
-  function addDivToContainer() {
+  function addDivToContainer(clickedId) {
       var newDiv = document.createElement("div");
       newDiv.setAttribute('class', 'message sender');
-      newDiv.textContent = "clicado";
+      switch (clickedId){
+        case "recepcao":
+        newDiv.textContent = "voce clicou recepcao";
+        break;
+
+        case "financeiro":
+        newDiv.textContent = "voce clicou financeiro";
+        break;
+
+        case "rh":
+        newDiv.textContent = "voce clicou recursos humanos";
+        break;
+
+        default:
+          "algo de errado"
+      }
       var container = document.getElementById("container");
       container.appendChild(newDiv);
+
   }
+
+  $( document ).ready(function() {
+    console.log( "ready!" );
+});
