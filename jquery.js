9a5d2a688df590563889e6aa653c1b68f0dadd6f
donmarx <marx.maciel2013@gmail.com>
2023-09-21 00:21:53 -0300

    new animations; updated jquery

--- jquery.js
+++ jquery.js
@@ -1,24 +1,22 @@
-//var recepcaoLabel = ["Agende uma Visita - Conheça o Colégio", "Achados e Perdidos", "Agendar Atendimento com Coordenação", "Atendimento sobre Dúvidas Gerais e Rotinas", "Atestados Médicos", "Entrega de Históricos Escolares", "Horários de Aulas", "Informações sobre as Crianças", "Inscrições e Dúvidas sobre Atividades Extras", "Liberação de Alunos com Terceiros", "Problemas com Usuário e Senha Portal/ Aplicativo"]
-//var recepcaoIds = ["visita", "achados", "atendimentoCoord", "atendimentoRotinas", "atestados", "historico", "horarios", "infoCriancas", "atvExtras", "libTerceiros", "problemasPortal"]
+var recepcaoLabel = ["Agende uma Visita - Conheça o Colégio", "Achados e Perdidos", "Agendar Atendimento com Coordenação", "Atendimento sobre Dúvidas Gerais e Rotinas", "Atestados Médicos", "Entrega de Históricos Escolares", "Horários de Aulas", "Informações sobre as Crianças", "Inscrições e Dúvidas sobre Atividades Extras", "Liberação de Alunos com Terceiros", "Problemas com Usuário e Senha Portal/ Aplicativo"]
+var recepcaoIds = ["visita", "achados", "atendimentoCoord", "atendimentoRotinas", "atestados", "historico", "horarios", "infoCriancas", "atvExtras", "libTerceiros", "problemasPortal"]
 
-//var financeiroLabel = ["Acordos Financeiros", "Atualização de Boletos Vencidos", "Boletos Unificados", "Declarações de Quitação De Débitos", "Dúvidas sobre Valores Cobrados", "Encaminhar Boletos", "Informação sobre Parcelas em Atraso", "Matrículas e Mensalidades Proporcionais"]
-//var financeiroIds = ["acordos", "boletosVencidos", "boletosUnificados", "quitacaoDebito", "duvidasValores", "encaminharBoletos", "parcelasAtraso", "matriculasProporcionais"]
+var financeiroLabel = ["Acordos Financeiros", "Atualização de Boletos Vencidos", "Boletos Unificados", "Declarações de Quitação De Débitos", "Dúvidas sobre Valores Cobrados", "Encaminhar Boletos", "Informação sobre Parcelas em Atraso", "Matrículas e Mensalidades Proporcionais"]
+var financeiroIds = ["acordos", "boletosVencidos", "boletosUnificados", "quitacaoDebito", "duvidasValores", "encaminharBoletos", "parcelasAtraso", "matriculasProporcionais"]
 
-//var rhLabel = ["Acolhimento do Candidato a Estágio", "Dúvidas Sobre Estágios", "Encaminhamento de Currículo"]
-//var rhIds = ["acolhimentoEstagio", "duvidaEstagio", "curriculo"]
+var rhLabel = ["Acolhimento do Candidato a Estágio", "Dúvidas Sobre Estágios", "Encaminhamento de Currículo"]
+var rhIds = ["acolhimentoEstagio", "duvidaEstagio", "curriculo"]
 
-//function createDefaultButtons (div) {
-//    var $newButtonOutro = $("<button>").text("Outro").attr("id", "outro");
-//    var $newButtonVoltar = $("<button>").text("Voltar").attr("id", "voltar");
-//
-//    div.append($newButtonOutro,$("<br>"));
-//    div.append($newButtonVoltar,$("<br>"));
-//}
+function createDefaultButtons (div) {
+    var $newButtonOutro = $("<button>").text("Outro").attr("id", "outro");
+    var $newButtonVoltar = $("<button>").text("Voltar").attr("id", "voltar");
 
-const { recepcaoLabel, recepcaoIds, financeiroLabel, financeiroIds, createDefaultButtons} = require("./buttonsList")
+    div.append($newButtonOutro,$("<br>"));
+    div.append($newButtonVoltar,$("<br>"));
+}
 
-$(document).ready(function() {
-    $("#recepcao").click(function() {
+jQuery(function() {
+    $("#recepcao").on("click",function() {
         //creates new div
         var $newDiv = $("<div>").addClass("message sender");
 
@@ -33,10 +31,10 @@ $(document).ready(function() {
 
         //appends to container
         $(".message-container").append($newDiv);
-
+        $(".message-container").animate({ scrollTop: $('.message-container').prop("scrollHeight")}, 1500);
     });
 
-    $("#financeiro").click(function() {
+    $("#financeiro").on("click",function() {
         //creates new div
         var $newDiv = $("<div>").addClass("message sender");
 
@@ -51,9 +49,29 @@ $(document).ready(function() {
 
         //appends to container
         $(".message-container").append($newDiv);
+        $(".message-container").animate({ scrollTop: $('.message-container').prop("scrollHeight")}, 1500);
     });
 
-    $("#rh").click(function() {
+    $("#rh").on("click",function() {
+        //creates new div
+        var $newDiv = $("<div>").addClass("message sender");
+
+        //creates the buttons
+        for(let i = 0; i<rhLabel.length ; i++){
+            var $newButton = $("<button>").text(rhLabel[i]).attr("id", rhIds[i]);
+
+            $newDiv.append($newButton,$("<br>"));
+        };
+
+        createDefaultButtons($newDiv);
+
+        //appends to container
+        $(".message-container").append($newDiv);
+        $(".message-container").animate({ scrollTop: $('.message-container').prop("scrollHeight")}, 1500);
+    });
+
+
+    $("#voltar").on("click",function() {
         //creates new div
         var $newDiv = $("<div>").addClass("message sender");
 
